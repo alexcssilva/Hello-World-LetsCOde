b52481428c7ddffaf5e88b02ae3e545d0bc26980
Alex Silva <alexcssilva@gmail.com>
2022-09-12 08:51:37 -0300

    Aula 02 concluida

--- hello-world/src/App.java
+++ hello-world/src/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+      int a;
+      int b = 2;
+      a = 3;
+      int soma = a + b;
+      int subtracao = a - b;
+      int multiplicacao = a * b;
+      float divisao = (float) a / b;
+
+      System.out.println(soma);
+      System.out.println(subtracao);
+      System.out.println(multiplicacao);
+      System.out.println(divisao);
+
     }
 }
